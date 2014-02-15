6e6127ad9e7c4e094cf7b7fc0b5b16c52c3f143f
Zach Wade <zacharywade@gmail.com>
2014-02-14 21:34:49 -0500

    initial commit

---
+++ min3d/min3d/Min3d.java
@@ -0,0 +1,19 @@
+package min3d;
+
+/*
+ * Project homepage: 	http://code.google.com/p/min3d
+ * License:				MIT
+ * 
+ * Author: 				Lee Felarca
+ * Website:				http://www.zeropointnine.com/blog
+ *
+ * Author: 				Dennis Ippel
+ * Author blog:			http://www.rozengain.com/blog/
+ */
+
+public class Min3d 
+{
+
+	public static final String TAG = "Min3D";
+	
+}
