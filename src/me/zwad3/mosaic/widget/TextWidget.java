ab408ef9188d9964d158e9a77fbc7632532cf6d8
Zach Wade <zacharywade@gmail.com>
2014-02-14 23:12:19 -0500

    update

--- src/me/zwad3/mosaic/widget/TextWidget.java
+++ src/me/zwad3/mosaic/widget/TextWidget.java
@@ -18,15 +18,18 @@ public class TextWidget implements Widget {
 	@Override
 	public Bitmap renderBitmap() {
 		 Paint paint = new Paint();
-		 paint.setTextSize(12);
-		 paint.setColor(Color.BLACK);
+		 paint.setTextSize(1);
+		 paint.setColor(Color.RED);
 		 paint.setTextAlign(Paint.Align.LEFT);
 		 int width = (int) (paint.measureText("Hello Texture") + 0.5f); // round
 		 float baseline = (int) (-paint.ascent() + 0.5f); // ascent() is negative
 		 int height = (int) (baseline + paint.descent() + 0.5f);
-		 Bitmap image = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+		 Bitmap image = Bitmap.createBitmap(256, 256, Bitmap.Config.ARGB_8888);
 		 Canvas canvas = new Canvas(image);
-		 canvas.drawText("Hello Texture", 0, baseline, paint);
+		 canvas.drawRect(0, 0, 256, 256, paint);
+		 paint.setColor(Color.BLACK);
+		 //drawText("Hello Texture", 0, 0, paint);
+		
 		 return image;
 	}
 	
